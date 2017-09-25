6b6ff3586b4a8a5ff6981a4c7d33d6ab62d53dc5
PC-MGM-SURFACE\zoolo <matthewmcmillian@outlook.com>
2017-09-25 18:56:36 -0500

    -Added Java HW3 for CS3345

---
+++ CS3345 - Data Structures/CS3345_HW3/src/main.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class main {
+
+	public static void main(String[] args) throws Exception {
+		FileReader fr = new FileReader("dictionary");
+	}
+
+}
