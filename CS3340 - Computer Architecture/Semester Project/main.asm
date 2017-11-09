6b7164db4e03bbe728210597166d83c0ce017d6d
Matthew McMillian <matthewgmcmillian@outlook.com>
2017-11-08 20:26:19 -0600

    -Updated Mips

--- CS3340 - Computer Architecture/Semester Project/main.asm
+++ CS3340 - Computer Architecture/Semester Project/main.asm
@@ -8,7 +8,7 @@ yellow:		.word	0x00FFFF00
 purple:		.word	0x00006400
 green:		.word	0x0000FFFF
 
-startAddress:   .word 	0x10010004
+startAddress:   .word 	0x10010000
 
 	.text
 	
@@ -90,8 +90,6 @@ create: # Displays a on a 1024x512 graph
 	add $t0, $zero, $zero	
 	add $t1, $zero, $zero
 	
-	li $t9, 512 # Normalize Value for Y-Values
-	
 	#  Sets the $S-Values to the new actual values
 	sub $s1, $t9, $s1
 	sub $s2, $t9, $s2
@@ -99,78 +97,50 @@ create: # Displays a on a 1024x512 graph
 	sub $s4, $t9, $s4	
 	
 	# Loading the Colors
-	la $t3, red
-	la $t4, yellow
-	la $t5, purple
-	la $t6, green
+	lw $t3, red
+	
+	# Bounds per house
+	li $t7, 0
+	li $t8, 0
 	
 	li $t0, 0 # Iterator
 	li $t2, 0 # Y Value
-	li $s4, 1024 # Moduluo Val
+	li $s5, 1024 # Moduluo Val
 	la $a0, startAddress # Address of the Board	
 	
 create_lp:
-	# if x (> 63, and < 239) and y (< 511, and > {511-number}), Draw Gryffindor House (RED)
-	# if x (> 303, and < 479) and y (< 511, and > {511-number}), Draw Hufflepuff House? (YELLOW)
-	# if x (> 543, and < 719) and y (< 511, and > {511-number}), Draw Ravenclaw House? (BLUE)
-	# if x (> 783, and < 959) and y (< 511, and > {511-number}), Draw Slytherin  House? (GREEN)
+	# Each Pixel -> 32x32 : 3 blocks per bar, recalculate: >>>
+	#	5 Sections of WhiteSpace:
+	#		Spanning 128 len long each (or 4 blocks)
 	
-	div $t0, $s4 # Moduluo to get X
+	# $t1 is x value
+	# $t2 is y value
+	
+	# Just draw based on position;
+	#	i.e. go to first col pos, draw row, repeat until col pos = 511	
+	
+	div $t0, $s5 # Moduluo to get X
 	mfhi $t1 # X Value
 	
-	gryph:
-		# if (x > 63 && x < 239)
-		bgt $t1, 239, huffl
-		blt $t1, 63, end
+	gryph: # Starts @ 128
 		
-		# if (y < 511, and y > {511-number})
-		bgt $t2, 511, end
-		blt $t2, $s1, end		
-		sb $t3, ($a0)
-		j end
+	# WhiteSpace starts @ 224
+		
+	huffl: # Starts @ 352
+		
+		
+	# WhiteSpace start @ 448
+			
+	raven: # Starts @ 576
+		
+	# WhiteSpace starts @ 672	
+		
+	slyth: # Starts @ 800
+		
+		
+	# WhiteSpace starts @ 896
 	
-	huffl:
-		# if (x > 303 && x < 479)
-		bgt $t1, 479, raven
-		blt $t1, 303, raven
-		
-		# if (y < 511, and y > {511-number})
-		bgt $t2, 511, end
-		blt $t2, $s2, end		
-		sb $t4, ($a0)
-		j end
-
-	raven:
-		# if (x > 543 && x < 719)
-		bgt $t1, 719, slyth
-		blt $t1, 543, slyth
-		
-		# if (y < 511, and y > {511-number})
-		bgt $t2, 511, end
-		blt $t2, $s3, end		
-		sb $t5, ($a0)
-		j end
-		
-	slyth:
-		# if (x > 783 && x959719)
-		bgt $t1, 959, end
-		blt $t1, 783, end
-		
-		# if (y < 511, and y > {511-number})
-		bgt $t2, 511, end
-		blt $t2, $s4, end		
-		sb $t6, ($a0)
-		j end
-		
 	end:
-		# End
-		bne $t1, 1023, noad # Checks the Next Row
-		addi $t1, $t1, 1 # Adds 1 to ty height
-	noad:
-		# Still on the same Y
-		addi $t0, $t0, 1
-		addi $a0, $a0, 4
-		j create_lp
 exit:
 	# Prints out the corresponding values per team
 	li $v0, 1
