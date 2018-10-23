b491a6a9a10f05f2f4764eda936fa42b7e403278
Matthew McMillian <matthewgmcmillian@gmail.com>
2018-10-23 16:49:18 -0500

    os done

--- CS4348 - Operating Systems/HotelProject/src/Classes/Bellhop.java
+++ CS4348 - Operating Systems/HotelProject/src/Classes/Bellhop.java
@@ -1,10 +1,16 @@
 package Classes;
 
-import interfaces.HotelHelper;
+import Globals.HotelHelper;
 
 import java.util.HashMap;
 import java.util.concurrent.Semaphore;
 
+/**
+ * @semaphoreHashMap - Global HashMap shared by all classes
+ * @bellhopGuestHashMap - Used to handle specific interactions between guests and bellhops
+ * @mutex - Used to make sure guests obtain the correct bellhop
+ * @toStringId, @guestNo, @beingUsed - Defining variables of the class
+ */
 public class Bellhop extends Thread {
     private HashMap<String, Semaphore> semaphoreHashMap;
     private HashMap<String, Semaphore> bellhopGuestHashMap;
@@ -13,11 +19,15 @@ public class Bellhop extends Thread {
 
     private String toStringId;
     private int guestNo;
-
-    private boolean beingUsed = false;
+    private int bellhopNo;
 
     private HotelHelper helper;
 
+    /**
+     * @param bellhopNo - The 'relative' bellhop number used in output
+     * @param helper    - Global helper function shared among all classes
+     * @throws InterruptedException - For semaphore.acquire()
+     */
     public Bellhop(Integer bellhopNo, HotelHelper helper) throws InterruptedException {
         this.helper = helper;
         this.mutex = new Semaphore(1, true);
@@ -25,25 +35,33 @@ public class Bellhop extends Thread {
 
         this.semaphoreHashMap = helper.initializeSemaphoreHashMap();
         this.toStringId = "Bellhop " + bellhopNo + ": ";
+        this.bellhopNo = bellhopNo;
         this.guestNo = -1;
     }
 
-    public boolean isUsed() {
-        return beingUsed;
+    public int getBellhopNo() {
+        return bellhopNo;
     }
 
-    public void setUsed(boolean beingUsed) {
-        this.beingUsed = beingUsed;
-    }
-
+    /**
+     * @param bellhopGuestHashMap - Sets the bellhopGuestHashMap of this thread to that
+     *                            of whatever is passed into it
+     */
     public void setBellhopGuestHashMap(HashMap<String, Semaphore> bellhopGuestHashMap) {
         this.bellhopGuestHashMap = bellhopGuestHashMap;
     }
 
+    /**
+     * @param guestNo - A guest number used to give a guest number to output
+     */
     public void setGuestNo(Integer guestNo) {
         this.guestNo = guestNo;
     }
 
+    /**
+     * Acts as a mutex, handles interaction between classes so that the guests and
+     * clerks get assigned correctly. Used outside of this class.
+     */
     public void triggerMutex() {
         if (mutex.availablePermits() == 0) {
             mutex.release();
@@ -56,26 +74,27 @@ public class Bellhop extends Thread {
         }
     }
 
+    /**
+     * Runs the thread, contains the main code for handling interactions between the objects.
+     */
     @Override
     public void run() {
         printStringToConsole(toStringId, "is ready to help a guest");
         while (this.isAlive()) {
             try {
                 this.semaphoreHashMap.get("lock").acquire();
-                this.beingUsed = false;
                 this.guestNo = -1;
                 this.bellhopGuestHashMap = null;
                 this.semaphoreHashMap.get("bellhops").release();
+                this.helper.addToBellhopQueue(this);
                 this.semaphoreHashMap.get("lock").release();
+
+                printStringToConsole(toStringId, "is ready to help a guest");
 
                 this.mutex.acquire();
 
                 printStringToConsole(toStringId, "has obtained the bags of [Guest ", guestNo + "", "]");
-                try{
-                    this.bellhopGuestHashMap.get("hasBags").release();
-                } catch (Exception e){
-                    printStringToConsole(toStringId, ", error at ", guestNo+"");
-                }
+                this.bellhopGuestHashMap.get("hasBags").release();
 
                 this.bellhopGuestHashMap.get("guestNeedsBags").acquire();
 
@@ -89,20 +108,28 @@ public class Bellhop extends Thread {
                 printStringToConsole(toStringId, "has gotten their tip and leaves from [Guest ", guestNo + "", "]");
 
                 this.bellhopGuestHashMap.get("gotTip").release();
-
-                printStringToConsole(toStringId, "is ready to help a guest");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * @return - toString for debug use only
+     */
     @Override
     public String toString() {
-        return toStringId + ", " + beingUsed;
+        return toStringId;
     }
 
-    private void printStringToConsole(String ... strings) {
+    /**
+     * @param strings - Variable Strings as input, allows for multiple input
+     *                <p>
+     *                This function concats the strings in an output message and prints at the
+     *                same time using StringBuilder so that the async nature of String concat is
+     *                held.
+     */
+    private void printStringToConsole(String... strings) {
         StringBuilder sb = new StringBuilder();
         for (String s : strings) {
             sb.append(s);
--- CS4348 - Operating Systems/HotelProject/src/Main.java
+++ CS4348 - Operating Systems/HotelProject/src/Main.java
@@ -1,25 +1,35 @@
 import Classes.Bellhop;
 import Classes.Clerk;
 import Classes.Guest;
-import interfaces.HotelHelper;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.concurrent.Semaphore;
+import Globals.HotelHelper;
 
 public class Main {
 
-    private static HashMap<String, Semaphore> semaphoreHashMap;
-
-    private static HotelHelper helper;
-
+    /**
+     * @param args - Input arguments to the program, none in this case
+     * @return - Nothing to return
+     * @throws InterruptedException - In case of semaphore error
+     *                              <p>
+     *                              Contains 4 variables:
+     * @guestCount - Integer variable that controls the number of guests that enter the hotel
+     * @clerkCount - Integer variable that controls the number of clerks at the hotel
+     * @bellhopCount - Integer variable that controls the number of bellhops at the hotel
+     * {@link HotelHelper} - A class that contains global functions and variables for the program
+     * <p>
+     * The program starts by generating new clerks and bellhops that are immediately started. The
+     * program then generates new guests but waits to start them. The clerks, guests, and bellhops
+     * all get added to a ArrayList of their respective types for handling inside the HotelHelper.
+     * Since all the objects are passed by reference in this case, all the subclasses
+     * (clerk, guest, bellhop) have access to HotelHelper. The guests then get started in a
+     * queue-like fashion.
+     */
     public static void main(String[] args) throws InterruptedException {
 
         int guestCount = 25;
         int clerkCount = 2;
         int bellhopCount = 2;
 
-        helper = new HotelHelper();
+        HotelHelper helper = new HotelHelper();
         helper.generateGlobalHashMap();
 
         for (int i = 0; i < clerkCount; i++) {
@@ -31,7 +41,10 @@ public class Main {
         }
 
         for (int i = 0; i < guestCount; i++) {
-            helper.addToGuests(new Guest(i + 1, helper));
+            Guest g = new Guest(i + 1, helper);
+            helper.addToGuests(g);
+            helper.addToGuestQueue(g);
+            helper.getGuest(i).start();
         }
 
         for (int i = 0; i < clerkCount; i++) {
@@ -42,11 +55,10 @@ public class Main {
             helper.getBellhop(i).start();
         }
 
-        for (int i = 0; i < guestCount; i++) {
-            helper.getGuest((i)).start();
-        }
 
-        Thread.sleep(1000);
+        for (int i = 0; i < guestCount; i++) {
+            helper.getGuest(i).join();
+        }
         System.out.println();
         System.out.println(helper.getTotalRetired());
         System.out.println(helper.getRetiredArrayList());
